238273020b8291ecaf00076a02a702f3235c1f1b
Tu-guang <1626406673@qq.com>
2023-04-14 22:56:04 +0800

    test

--- server/router/login.js
+++ server/router/login.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
+const connect = require('../db/mysql');
 router.post('/login', (req, res) => {
   console.log(req.body);
-  res.send('ok');
+  connect.query(
+    'SELECT * FROM users WHERE username = ? AND password = ?',
+    [req.body.username, req.body.password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({ err: 500, msg: '错误' });
+      } else {
+        if (result.length !== 0) {
+          res.send({ code: 200, msg: '登录成功' });
+        } else {
+          res.send({ err: 500, msg: '账号密码错误' });
+        }
+      }
+    }
+  );
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const app = express()
-const bodyparser = require('body-parser')
-
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const login = require('./router/login');
 //extended:false 表示使用系统模块querystring来处理 将字符串转化为对象
 app.use(
   bodyparser.urlencoded({
     extended: true,
   })
-)
-app.use(bodyparser.json())
+);
+app.use(bodyparser.json());
 //挂载内置中间件处理静态文件
-app.use(express.static('public'))
+app.use(express.static('public'));
 // express框架解决跨域问题的代码，注意该代码要放在 app.use(router); 之前
 //设置跨域请求
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Content-Type, Content-Length, Authorization,Token,Accept, X-Requested-With , yourHeaderFeild'
-  )
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-  res.header('X-Powered-By', ' 3.2.1')
-  res.header('Content-Type', 'application/json;charset=utf-8')
-  next()
-})
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('X-Powered-By', ' 3.2.1');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 
 app.get('/', function(req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 //登录
-app.use('/api', login)
+app.use('/api', login);
 let server = app.listen(8081, function() {
-  let port = server.address().port
-  console.log('应用实例，访问地址为 http://localhost:%s', port)
-})
+  let port = server.address().port;
+  console.log('应用实例，访问地址为 http://localhost:%s', port);
+});
